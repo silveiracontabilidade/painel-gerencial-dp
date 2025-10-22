511c16399c237de5f4f7edb8e7e48b4371512cdb
Denis Camargo <denis.camargo@silveiracontabilidade.com.br>
2025-10-22 15:30:37 -0300

    ajuste agenda menu producao

--- frontend/src/components/Header/index.js
+++ frontend/src/components/Header/index.js
@@ -82,7 +82,7 @@ const Header = () => {
               <span className="menu__title">Cadastros</span>
               {menuAberto === 'cadastros' && (
                 <ul className="submenu">
-                  <li><NavLink to="/agenda">Agenda</NavLink></li>
+                  {/* <li><NavLink to="/agenda">Agenda</NavLink></li> */}
                   <li><NavLink to="/grupos">Grupos</NavLink></li>
                   <li><NavLink to="/motivos-rescisao">Tipo Aviso Prévio</NavLink></li>
                   <li><NavLink to="/tipos-admissao">Tipos de Admissão</NavLink></li>
